d96c4287ae6d64aedc337799216807c157484d63
Rafael Feliciano <rafaelfelic@gmail.com>
2024-10-24 11:25:01 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Every line of code I write brings me closer to my goal: becoming the best developer in the world. I believe that passion, perseverance, and continuous learning are the keys to transforming ideas into reality. Let's push boundaries, break limits, and build the future, one code at a time. Join me on this journey to make an impact and innovate for a better world.</h1>
+}
+
+export default Home;
