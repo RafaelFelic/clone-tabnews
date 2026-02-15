6cf5e6b1e517e2694b450f2fd12efb3ad4db530b
Rafael Feliciano <rafaelfelic@gmail.com>
2026-02-15 05:07:53 +0000

    add `database.js` and `.env` files

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,4 @@
-import { useState, useEffect } from "react";
-
 function Home() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch("/api/v1/status")
-      .then((response) => response.json())
-      .then((responseData) => {
-        console.log(responseData);
-        setData(responseData);
-      });
-  }, []);
-
   return (
     <div>
       <h1>Bem-vindo ao Clone do TabNews!</h1>
@@ -19,13 +6,6 @@ function Home() {
         Este é um projeto de clone do TabNews, uma plataforma de notícias e
         discussões.
       </p>
-
-      {data && (
-        <div>
-          <h2>Dados da API:</h2>
-          <pre>{data.chave?.toUpperCase()}</pre>
-        </div>
-      )}
     </div>
   );
 }
